849836aae917c4c23f051875bad90e3b15396bd9
ArielTapia <arieltapia42@gmail.com>
2024-04-17 20:46:14 -0300

    Actualizacion

--- Codificacion/Semana2/ejemplo2.cpp
+++ Codificacion/Semana2/ejemplo2.cpp
@@ -4,18 +4,22 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    // int/int = int
-    // float/int = float
-    // int/float = float
-    // float/float = float
+    //   int/int    = int
+    // float/int    = float
+    //   int/float  = float
+    // float/float  = float
 
-    cout << "Division entera " << (float)7/5 << endl;
+    cout << "Division entera 7/5 " << 7 / 5 << endl;
 
-    cout << "Division real 7.0/5 " << 7.0/5 << endl;
+    cout << "Division real 7.0/5 " << 7.0 / 5 << endl;
 
-    cout << "Division real 7/5.0 " << 7/5.0 << endl;
+    cout << "Division real 7/5.0 " << 7 / 5.0 << endl;
 
-     cout << "Division real 7.0/5.0 " << 7.0/5.0 << endl;
+    cout << "Division real 7.0/5.0 " << 7.0 / 5.0 << endl;
+
+    /// CASTEO
+
+    cout << "Division entera 7/5 " << (float)7/5 << endl;
 
     return 0;
 }
